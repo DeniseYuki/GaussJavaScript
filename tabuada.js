18d4baba2386d0fb2e68e06f1945e567ebc37f43
DeniseYuki <49623035+DeniseYuki@users.noreply.github.com>
2023-04-25 17:10:47 -0300

    Update tabuada.js

--- tabuada.js
+++ tabuada.js
@@ -3,8 +3,9 @@ var i =0;
 var j;
 document.write("Digite um valor para a tabuada");
 j = prompt();
+document.write("---------------------------------------------------");
 while(i<11)
 {
-    document.write(i*j + " ");
+    document.write(i*j);
     i++;
 }
